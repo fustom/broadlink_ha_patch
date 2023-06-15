a8dc594bfe088f194539767e2d234bc320ddbc05
fustom <fustom@gmail.com>
2023-06-15 10:43:14 +0200

    Use python 3.11

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 CONTAINER=$(docker container ls | grep home-assistant | cut -f 1 -d ' ')
-docker cp ./br_climate.py $CONTAINER:/usr/local/lib/python3.10/site-packages/broadlink/climate.py
+docker cp ./br_climate.py $CONTAINER:/usr/local/lib/python3.11/site-packages/broadlink/climate.py
 docker cp ./ha_climate.py $CONTAINER:/usr/src/homeassistant/homeassistant/components/broadlink/climate.py
 docker cp ./const.py $CONTAINER:/usr/src/homeassistant/homeassistant/components/broadlink/const.py
 docker cp ./updater.py $CONTAINER:/usr/src/homeassistant/homeassistant/components/broadlink/updater.py
