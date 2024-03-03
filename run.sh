029d37657bbe8fb8db2798b4eb51fe4ff4cd2a76
fustom <fustom@gmail.com>
2024-03-03 11:13:18 +0100

    Remove unneeded py

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 CONTAINER=$(docker container ls | grep home-assistant | cut -f 1 -d ' ')
 docker cp ./br_climate.py $CONTAINER:/usr/local/lib/python3.12/site-packages/broadlink/climate.py
 docker cp ./ha_climate.py $CONTAINER:/usr/src/homeassistant/homeassistant/components/broadlink/climate.py
-docker cp ./const.py $CONTAINER:/usr/src/homeassistant/homeassistant/components/broadlink/const.py
-docker cp ./updater.py $CONTAINER:/usr/src/homeassistant/homeassistant/components/broadlink/updater.py
-docker cp ./manifest.json $CONTAINER:/usr/src/homeassistant/homeassistant/components/broadlink/manifest.json
